357413639927b972c3321eeb22174b31bf499a8d
Bruno Lima <ibrunoliimas@gmail.com>
2023-08-09 02:44:07 -0300

    Adicionando logo no menu mobile

--- src/app/components/ui/menu/menu.tsx
+++ src/app/components/ui/menu/menu.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { IoClose } from "react-icons/io5";
 import { links } from "./links";
+import Image from "next/image";
 
 
 // const links = ["features", "pricing", "contact"];
@@ -12,9 +13,21 @@ type MenuProps = {
 export const Menu = ({ onClose }: MenuProps) => {
     return (
         <div className="fixed z-50 top-0 left-0 w-full h-screen p-8 flex flex-col items-center justify-start bg-neutral-950 text-white opacity-[.98]">
-            <ul className="pt-10 w-full flex flex-col items-center justify-center">
+            <div className="pt-10 mb-4">
+                <Link
+                    href="/">
+                    <Image
+                        className=""
+                        src='/logo-gustavo_alt_2-white.svg'
+                        width={180}
+                        height={73.3}
+                        alt="Logo Gustavo"
+                    />
+                </Link>
+            </div>
+            <ul className="w-full flex flex-col">
                 {links.map((link, index) => (
-                    <li key={index} className="flex items-center py-4 px-2 border-b border-b-neutral-800 w-full">
+                    <li key={index} className="py-4 px-2 border-b border-b-neutral-800">
                         <Link
                             className="text-white capitalize text-lg transition-all duration-300"
                             href={link.href}
@@ -23,7 +36,7 @@ export const Menu = ({ onClose }: MenuProps) => {
                         </Link>
                     </li>
                 ))}
-                
+
             </ul>
             <button className="absolute top-8 right-8 text-white" onClick={onClose}>
                 <IoClose size={50} />
